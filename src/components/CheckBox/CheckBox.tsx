875bde5b70376a61ddb783105b825c51a93da8b9
CodeDepo <codedepon@gmail.com>
2022-09-22 14:20:35 +0200

    1:1 aspect ratio added

--- src/components/CheckBox/CheckBox.tsx
+++ src/components/CheckBox/CheckBox.tsx
@@ -6,13 +6,19 @@ import styles from "./CheckBox.module.scss";
 import ContentBox from "../ContentBox/ContentBox";
 interface CheckBoxProps {
   checked?: boolean;
+  size?: string;
 }
-function CheckBox({ checked }: CheckBoxProps) {
+function CheckBox({ checked, size }: CheckBoxProps) {
   const [isChecked, setIsChecked] = useState(Boolean(checked));
   const toggleChecked = () => setIsChecked(!isChecked);
   const checkmarkRef = useRef(null);
   return (
-    <ContentBox onClick={toggleChecked} className={styles.checkbox}>
+    <ContentBox
+      onClick={toggleChecked}
+      // @ts-ignore
+      size={size || "2em"}
+      className={styles.checkbox}
+    >
       <button className={styles.innerBox}>
         {isChecked && (
           <CheckmarkIcon ref={checkmarkRef} className={styles.checkmark} />
